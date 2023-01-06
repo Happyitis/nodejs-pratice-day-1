7f6d904e2c2b0f022266e34aa6ab57723e06d1dc
ハッピーHappy <aayush.maharjan.499@gmail.com>
2023-01-06 09:35:53 +0545

    uploading nodejs files

---
+++ index.js
@@ -0,0 +1,16 @@
+let value ="hello world";
+console.log(value);
+
+
+const AnotherValue = "This is constant value";
+console.log(AnotherValue);
+
+
+value = "IIMS";
+console.log(value);
+
+// AnotherValue = "changed value";
+// console.log(AnotherValue);                   (cannot chance constatnt value )
+
+const x = 1;
+console.log(typeof x);
